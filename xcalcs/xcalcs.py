f2fb4aac70f1665bf3dae095c3ba8de70c7aea8f
Gustavo Vilela Vargas <xgvargas@gmail.com>
2016-01-15 21:59:28 -0200

    using python 3 now

--- xcalcs/xcalcs.py
+++ xcalcs/xcalcs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
@@ -25,9 +25,9 @@ class MyApplication(QtGui.QWidget, Ui_Form, smartsignal.SmartSignal):
         # self.cfg = QSettings('oi', 'carcamano')
         # self.restoreGeometry(self.cfg.value('geometry'))
         #self.restoreState(self.cfg.value('state'))
-        #print 'lendo bunda:',self.cfg.value('bunda', type=int)
+        #print('lendo bunda:',self.cfg.value('bunda', type=int))
 
-        self._display_bottom()
+        self._display_to_bottom()
 
         self.editing = None
         self.nuns = [0]*32
@@ -39,7 +39,7 @@ class MyApplication(QtGui.QWidget, Ui_Form, smartsignal.SmartSignal):
         e.accept()
 
     def keyPressEvent(self, e):
-        # print e.key(), e.nativeModifiers(), e.nativeScanCode(), e.nativeVirtualKey()
+        # print(e.key(), e.nativeModifiers(), e.nativeScanCode(), e.nativeVirtualKey())
         c = chr(e.key()) if e.key() < 255 else ''
         shorts = (
                     ('+', self.btn_f_add),
@@ -66,18 +66,18 @@ class MyApplication(QtGui.QWidget, Ui_Form, smartsignal.SmartSignal):
 
         if not self.editing:
             if e.key() == QtCore.Qt.Key_Escape:
-                print 'limpa tudo'
+                print('limpa tudo')
             elif e.key() == QtCore.Qt.Key_Backspace:
-                print 'apaga uma linha'
+                print('apaga uma linha')
             elif e.key() == QtCore.Qt.Key_Enter or e.key() == QtCore.Qt.Key_Return:
-                print 'duplica'
+                print('duplica')
         else:
             if e.key() == QtCore.Qt.Key_Escape:
-                print 'para edit'
+                print('para edit')
             elif e.key() == QtCore.Qt.Key_Backspace:
-                print 'volta edit'
+                print('volta edit')
             elif e.key() == QtCore.Qt.Key_Enter or e.key() == QtCore.Qt.Key_Return:
-                print 'confirma'
+                print('confirma')
             elif c == 'E':
                 pass
             elif c == 'G':
@@ -90,47 +90,46 @@ class MyApplication(QtGui.QWidget, Ui_Form, smartsignal.SmartSignal):
             else:
                 #entra modo edicao
                 self.editing = True
-            print 'numero'
+            print('numero')
             self._showNuns()
 
     def _showNuns(self):
         txt = '''<html><head><style type="text/css">
 p {margin: 0px 0px; -qt-block-indent:0; text-indent:0px; white-space: pre-wrap; text-align: right;}
-body {font-family:'MS Shell Dlg 2'; font-size:9pt; font-weight:400; font-style:normal;}
+body {font-family:'Courier New'; font-size:9pt; font-weight:400; font-style:normal;}
 </style></head><body>'''
 
         for n in self.nuns:
             txt += '<p align="right">{}</p>'.format('nhaaa')   #<span style=" color:#ff0000;">asdqweqweasd</span>
 
-            self.edt_num.setHtml(txt+'</body></html>')
+        self.edt_num.setHtml(txt+'</body></html>')
+        self._display_to_bottom()
 
-            self._display_bottom()
-
-    def _display_bottom(self):
+    def _display_to_bottom(self):
             b = self.scr_display.verticalScrollBar()
             b.setValue(b.maximum())
 
     def _on_btn_angle__clicked(self):
-        print '_on_btn_angle__clicked'
+        print('_on_btn_angle__clicked')
 
     def _on_btn_format__clicked(self):
-        print '_on_btn_format__clicked'
+        print('_on_btn_format__clicked')
 
     def _on_btn_coord__clicked(self):
-        print '_on_btn_coord__clicked'
+        print('_on_btn_coord__clicked')
 
     def _on_btn_solver__toggled(self):
-        print '_on_btn_solver__toggled', self.sender().isDown(), self.sender().isChecked()
+        print('_on_btn_solver__toggled', self.sender().isDown(), self.sender().isChecked())
 
     def _on_btn_conv__toggled(self):
-        print '_on_btn_conv__toggled', self.sender().isDown(), self.sender().isChecked()
+        print('_on_btn_conv__toggled', self.sender().isDown(), self.sender().isChecked())
 
     def _on_btn_base__toggled(self):
-        print '_on_btn_base__toggled', self.sender().isDown(), self.sender().isChecked()
+        print('_on_btn_base__toggled', self.sender().isDown(), self.sender().isChecked())
 
     _funcoes = '`btn_f_.+`'
     def _when_funcoes__clicked(self):
-        print self.sender().objectName()
+        print(self.sender().objectName())
 
 
 if __name__ == "__main__":
